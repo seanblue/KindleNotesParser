667f89d8eba6435abadc90ae1ad4766cebd3316b
= <5884067+seanblue@users.noreply.github.com>
2021-03-20 16:07:14 -0400

    Allow a file to be selected and parsed into raw clippings

---
+++ KindleNotes/KindleNotes/Models/KindleClippingsFile.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace KindleNotes.Models
+{
+	public class KindleHighlight
+	{
+		public string Title;
+		public int StartPosition;
+		public int EndPosition;
+		public string DateAdded;
+		public string Content;
+	}
+	
+	public class KindleNote
+	{
+		public string Title;
+		public int Position;
+		public string DateAdded;
+		public string Content;
+	}
+
+	public class RawKindleClipping
+	{
+		private const string clippingDelimiter = "==========";
+		private readonly List<string> lines = new();
+		
+		public bool FullyInitialized;
+
+		public void AddLine(string line)
+		{
+			if (line == clippingDelimiter)
+				FullyInitialized = true;
+			else if (!string.IsNullOrWhiteSpace(line))
+				lines.Add(line);
+		}
+
+		public List<string> Lines => lines;
+	}
+
+	public class KindleClippingsFile
+	{
+
+		public IBrowserFile BrowserFile;
+		public bool FileHasBeenParsed;
+
+		public List<RawKindleClipping> rawClippings = new();
+		private RawKindleClipping currentClipping;
+
+		internal async Task ParseFile()
+		{
+			await ReadFileContent();
+			ProcessFileContent();
+
+			FileHasBeenParsed = true;
+		}
+
+		private async Task ReadFileContent()
+		{
+			rawClippings.Clear();
+
+			currentClipping = new RawKindleClipping();
+			string line;
+			var reader = new StreamReader(BrowserFile.OpenReadStream());
+
+			while ((line = await reader.ReadLineAsync()) is not null)
+			{
+				currentClipping.AddLine(line);
+				if (currentClipping.FullyInitialized)
+				{
+					rawClippings.Add(currentClipping);
+					currentClipping = new RawKindleClipping();
+				}
+			}
+		}
+
+		private void ProcessFileContent()
+		{
+			
+		}
+	}
+}
---
+++ KindleNotes/KindleNotes/Pages/Index.razor.cs
@@ -0,0 +1,27 @@
+﻿using KindleNotes.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KindleNotes.Pages
+{
+	public partial class Index
+	{
+		protected readonly Dictionary<string, object> supportedFileTypesAttribute = new()
+		{
+			{ "accept", ".txt" }
+		};
+
+		protected KindleClippingsFile kindleClippingsFile = new();
+
+		protected void SetFile(InputFileChangeEventArgs e)
+		{
+			kindleClippingsFile.BrowserFile = e.File;
+		}
+
+		protected async Task Submit()
+		{
+			await kindleClippingsFile.ParseFile();
+		}
+	}
+}
