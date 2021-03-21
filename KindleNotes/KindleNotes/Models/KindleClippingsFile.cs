110091974d4c22c2a2130a61f6351c84bb2871c3
= <5884067+seanblue@users.noreply.github.com>
2021-03-20 20:38:22 -0400

    Add basic error handling

--- KindleNotes/KindleNotes/Models/KindleClippingsFile.cs
+++ KindleNotes/KindleNotes/Models/KindleClippingsFile.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -7,29 +8,46 @@ namespace KindleNotes.Models
 {
 	public class KindleClippingsFile
 	{
-		private const long maxFileSizeBytes = 10_485_760; // 10MB
+		private const int numberOfMb = 2;
+		private const long maxFileSizeBytes = numberOfMb * 1_024 * 1_024;
 
 		public IBrowserFile BrowserFile;
-		public bool FileHasBeenParsed;
+		public string ErrorMessage;
 
 		public List<RawKindleClipping> rawClippings = new();
 		private RawKindleClipping currentClipping;
 
 		internal async Task ParseFile()
 		{
-			await ReadFileContent();
-			ProcessFileContent();
+			Reset();
 
-			FileHasBeenParsed = true;
+			try
+			{
+				await ReadFileContent();
+			}
+			catch (IOException)
+			{
+				ErrorMessage = $"The file exceeded the max length of {numberOfMb}MB.";
+			}
+			catch (Exception)
+			{
+				ErrorMessage = "An unknown error has occurred while reading the file.";
+			}
+
+			ProcessFileContent();
+		}
+
+		private void Reset()
+		{
+			rawClippings.Clear();
+			currentClipping = new RawKindleClipping();
+			ErrorMessage = null;
 		}
 
 		private async Task ReadFileContent()
 		{
-			rawClippings.Clear();
-
-			currentClipping = new RawKindleClipping();
 			string line;
-			var reader = new StreamReader(BrowserFile.OpenReadStream(maxFileSizeBytes));
+			using var reader = new StreamReader(BrowserFile.OpenReadStream(maxFileSizeBytes));
 
 			while ((line = await reader.ReadLineAsync()) is not null)
 			{
